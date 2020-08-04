a589256254f0c10c2fe746353ac810b41e1bf6e7
jennethydyrova <jennethydyrovaa@gmail.com>
2020-08-04 17:03:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const mod = 34 % 5;
+
+const max = Math.max(10, 13, 5, 20);
