637f6efd81769e3fbe4524d3e7712ef8010c3124
Cihan_Uygur <magicihan89@gmail.com>
2020-08-05 16:37:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 4;
+let num2 = 15.5;
+let multiply =  num1 * num2;
+
+let random = Math.random() + 1;
+
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; 
+
+
+let max = Math.max(1, 8, 3, 20);
+
