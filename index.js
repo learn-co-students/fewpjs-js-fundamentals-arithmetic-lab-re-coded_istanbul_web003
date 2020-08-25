c2f7ebf5b0e191aa5aa1522e19805c3fb7f199b8
gizemcdeniz <gizemc.deniz@gmail.com>
2020-08-25 16:58:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//First
+    const num1 = 60;
+    const num2 = 2;
+    const multiply = num1 + num2;
 
-// Write your code here
+console.log(multiply);
+
+//Second
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+
+  //Third
+  const number1 = 10;
+  const number2 = 6;
+  const mod = number1 % number2;
+  console.log(mod);
+
+  //Fourth
+
+  const max = Math.max(5, 20);
+  console.log(max);
