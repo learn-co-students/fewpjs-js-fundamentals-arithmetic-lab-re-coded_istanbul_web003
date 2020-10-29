03131d712617dab055dd1b6bd1d1b2d5cbc8a1b9
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-10-30 00:38:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10000) + 1;
+
+
+const n1 = 14;
+const n2 = 10;
+const mod = n1 % n2;
+
+const max = Math.max(18, 20);
